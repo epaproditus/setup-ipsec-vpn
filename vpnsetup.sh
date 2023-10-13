d8d967c6d9e461353c8fb80aa482d40ef4e9187e
epaproditus <72703910+epaproditus@users.noreply.github.com>
2023-10-13 06:53:29 -0500

    changed base URL

--- vpnsetup.sh
+++ vpnsetup.sh
@@ -245,8 +245,8 @@ install_pkgs() {
 }
 
 get_setup_url() {
-  base_url1="https://raw.githubusercontent.com/hwdsl2/setup-ipsec-vpn/master"
-  base_url2="https://gitlab.com/hwdsl2/setup-ipsec-vpn/-/raw/master"
+  base_url1="https://raw.githubusercontent.com/epaproditus/setup-ipsec-vpn/master"
+  base_url2="https://raw.githubusercontent.com/epaproditus/setup-ipsec-vpn/master"
   sh_file="vpnsetup_ubuntu.sh"
   if [ "$os_type" = "centos" ] || [ "$os_type" = "rhel" ] || [ "$os_type" = "rocky" ] \
     || [ "$os_type" = "alma" ] || [ "$os_type" = "ol" ]; then
